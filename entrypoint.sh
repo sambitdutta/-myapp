8eb87131e38d8f356253208dd70d3568b5f08e1e
Sambit Dutta <sambitdutta@Sambits-MacBook-Pro.local>
2019-06-27 21:23:12 +0530

    initial setup

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /myapp/tmp/pids/server.pid
+
+until nc -z -v -w30 $POSTGRES_HOST 5432
+do
+  >&2 echo "PostgreSQL is unavailable - sleeping"
+  # wait for 5 seconds before check again
+  sleep 5
+done
+
+>&2 echo "PostgreSQL is up - executing command"
+
+bundle exec rake db:setup --trace
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
