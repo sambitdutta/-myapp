a2cf42299fc90646915eb10dec5a73b5d39d2023
Sambit Dutta <sambitdutta@Sambits-MacBook-Pro.local>
2019-06-29 18:44:00 +0530

    asset dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery/dist/jquery.min
 //= require activestorage
-//= require turbolinks
+// require turbolinks
+//= require bootstrap/dist/js/bootstrap.min
+//= require bootstrap-notify/bootstrap-notify.min
 //= require_tree .
