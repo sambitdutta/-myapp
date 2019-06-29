db9ca44afb47e257fa1e0b4097ce938d832fb1d9
Sambit Dutta <sambitdutta@Sambits-MacBook-Pro.local>
2019-06-29 18:42:12 +0530

    docker, database, and asset dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ done
 
 >&2 echo "PostgreSQL is up - executing command"
 
-bundle exec rake db:setup --trace
+bundle exec rake db:create --trace && bundle exec rake db:migrate --trace
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
