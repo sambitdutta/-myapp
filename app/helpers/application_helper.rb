02cb5a229174ecd07a260c5d3b9e567318b6d199
Sambit Dutta <sambitdutta@Sambits-MacBook-Pro.local>
2019-06-29 19:37:05 +0530

    for displaying flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level.to_s
+    when 'notice' then "alert alert-info"
+    when 'success' then "alert alert-success"
+    when 'error', 'alert' then "alert alert-danger"
+    end
+  end
+
+  def flash_message(msg)
+    if msg.is_a? String
+      msg
+    elsif msg.is_a? Array
+      content_tag :ul do
+        msg.map do |m|
+          concat(content_tag :li, m)
+        end
+      end
+    end
+  end
 end
