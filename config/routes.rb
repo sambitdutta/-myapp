4835858432cec821f24590880220d8fcc987604d
Sambit Dutta <sambit.dutta@outlook.com>
2019-06-30 14:31:18 +0530

    routes defined

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
+
+  resources :users, only: %i[new create edit update] do
+    resources :messages, only: %i[index]
+  end
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  root to: "home#index"
+
+  get 'users/:id', to: 'users#show', constraints: { id: /[0-9a-zA-Z\.\-\_]+/ }
+
+  resources :password_resets, only: %i[new create show update]
 end
